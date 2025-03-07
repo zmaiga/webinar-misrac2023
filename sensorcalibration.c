b736efb993882a4ce060c5f3773a55d807216c28
zak <zacco11@hotmail.com>
2025-03-07 10:59:38 +0100

    Fixing link errors by uncommenting portions of source code

--- sensorcalibration.c
+++ sensorcalibration.c
@@ -2,37 +2,37 @@
 #include "thermalmod.h"
 #include "consts.h"
 
-// extern thrd_t calibration_th, reading_th;
-// extern mtx_t mutex_calib;
-// extern mtx_t mutex_read;
+extern thrd_t calibration_th, reading_th;
+extern mtx_t mutex_calib;
+extern mtx_t mutex_read;
 
-// void* periodic_sensors_callibaration(void * ptr)
-// {
-//     while (true) {
-//         mtx_lock(&mutex_calib);
-//         mtx_lock(&mutex_read);
-//         callibrate_sensors(ALL_SENSORS);
-//         sleep(500 * 1000);
-//         mtx_unlock(&mutex_read);
-//         mtx_unlock(&mutex_calib);
-//     }
-//     return NULL;
-// }
+void* periodic_sensors_callibaration(void * ptr)
+{
+    while (true) {
+        mtx_lock(&mutex_calib);
+        mtx_lock(&mutex_read);
+        callibrate_sensors(ALL_SENSORS);
+        sleep(500 * 1000);
+        mtx_unlock(&mutex_read);
+        mtx_unlock(&mutex_calib);
+    }
+    return NULL;
+}
 
-// void callibrate_sensors(SENSORS sensors) {
-//     switch (sensors)
-//     {
-//         case ALL_SENSORS:
-//             break;
-//         case UPPER_SENSOR:
-//             break;
-//         case LOWER_SENSOR:
-//             break;
-//         case LEFT_SENSOR:
-//             break;
-//         case RIGHT_SENSOR:
-//             break;
-//         default:
-//             break;
-//     }
-// }
+void callibrate_sensors(SENSORS sensors) {
+    switch (sensors)
+    {
+        case ALL_SENSORS:
+            break;
+        case UPPER_SENSOR:
+            break;
+        case LOWER_SENSOR:
+            break;
+        case LEFT_SENSOR:
+            break;
+        case RIGHT_SENSOR:
+            break;
+        default:
+            break;
+    }
+}
--- thermalmod.c
+++ thermalmod.c
@@ -10,86 +10,86 @@
 #include "thermalmod.h"
 #include "consts.h"
 
-// /* calibration and sensors scanning threads */
-// thrd_t calibration_th, reading_th;
+/* calibration and sensors scanning threads */
+thrd_t calibration_th, reading_th;
 
-// /* callibration and scanning require exclusive access to sensors */
-// mtx_t mutex_calib;
-// mtx_t mutex_read;
+/* callibration and scanning require exclusive access to sensors */
+mtx_t mutex_calib;
+mtx_t mutex_read;
 
-// /* Thermal state structure */
-// typedef struct temp_sensors {
-//  uint16_t upper_sensor;
-//  uint16_t lower_sensor;
-//  uint16_t left_sensor;
-//  uint16_t right_sensor;
-// } temp_sensors;
+/* Thermal state structure */
+typedef struct temp_sensors {
+ uint16_t upper_sensor;
+ uint16_t lower_sensor;
+ uint16_t left_sensor;
+ uint16_t right_sensor;
+} temp_sensors;
 
-// /* Current thermal state */
-// _Atomic temp_sensors thermal_state;
+/* Current thermal state */
+_Atomic temp_sensors thermal_state;
 
-// /* Sensors HW access */
-// static temp_sensors get_thermal_state_hw();
+/* Sensors HW access */
+static temp_sensors get_thermal_state_hw();
 
-// /* */
-// void update_thermal_state()
-// {
-//     temp_sensors current_state = get_thermal_state_hw();
-//     thermal_state.upper_sensor = current_state.upper_sensor;
-//     thermal_state.lower_sensor = current_state.lower_sensor;
-//     thermal_state.left_sensor = current_state.left_sensor;
-//     thermal_state.right_sensor = current_state.right_sensor;
-// }
+/* */
+void update_thermal_state()
+{
+    temp_sensors current_state = get_thermal_state_hw();
+    thermal_state.upper_sensor = current_state.upper_sensor;
+    thermal_state.lower_sensor = current_state.lower_sensor;
+    thermal_state.left_sensor = current_state.left_sensor;
+    thermal_state.right_sensor = current_state.right_sensor;
+}
 
-// /* read data from senors every 10 seconds*/
-// void * periodic_sensors_scan(void * ptr)
-// {
-//     while (true) {
-//         mtx_lock(&mutex_read);
-//         mtx_lock(&mutex_calib);
-//         read_sensors(ALL_SENSORS);
-//         sleep(10);
-//         mtx_unlock(&mutex_calib);
-//         mtx_unlock(&mutex_read);
-//     }
-//     return NULL;
-// }
+/* read data from senors every 10 seconds*/
+void * periodic_sensors_scan(void * ptr)
+{
+    while (true) {
+        mtx_lock(&mutex_read);
+        mtx_lock(&mutex_calib);
+        read_sensors(ALL_SENSORS);
+        sleep(10);
+        mtx_unlock(&mutex_calib);
+        mtx_unlock(&mutex_read);
+    }
+    return NULL;
+}
 
-// void initialize()
-// {
-//     int rc_cal, rc_read;
+void initialize()
+{
+    int rc_cal, rc_read;
 
-//     rc_read = thrd_create(&reading_th, (thrd_start_t) periodic_sensors_scan, (void *) NULL);
-//     rc_cal  = thrd_create(&calibration_th, (thrd_start_t) periodic_sensors_callibaration, (void *)NULL);
+    rc_read = thrd_create(&reading_th, (thrd_start_t) periodic_sensors_scan, (void *) NULL);
+    rc_cal  = thrd_create(&calibration_th, (thrd_start_t) periodic_sensors_callibaration, (void *)NULL);
     
-//     if (rc_cal == thrd_error || rc_read == thrd_error) {
-//         printf("ERORR; thrd_create() call failed\n");
-//         exit(EXIT_FAILURE);
-//     }
-// }
+    if (rc_cal == thrd_error || rc_read == thrd_error) {
+        printf("ERORR; thrd_create() call failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
-// temp_sensors get_thermal_state_hw() {
-//     temp_sensors sensors;
-//     return sensors;
-// }
+temp_sensors get_thermal_state_hw() {
+    temp_sensors sensors;
+    return sensors;
+}
 
-// void read_sensors(SENSORS sensors) {
-//     switch (sensors)
-//     {
-//         case ALL_SENSORS:
-//             break;
-//         case UPPER_SENSOR:
-//             break;
-//         case LOWER_SENSOR:
-//             break;
-//         case LEFT_SENSOR:
-//             break;
-//         case RIGHT_SENSOR:
-//             break;
-//         default:
-//             break;
-//     }
-// }
+void read_sensors(SENSORS sensors) {
+    switch (sensors)
+    {
+        case ALL_SENSORS:
+            break;
+        case UPPER_SENSOR:
+            break;
+        case LOWER_SENSOR:
+            break;
+        case LEFT_SENSOR:
+            break;
+        case RIGHT_SENSOR:
+            break;
+        default:
+            break;
+    }
+}
 
 /*
  * Grabs input from stdin
